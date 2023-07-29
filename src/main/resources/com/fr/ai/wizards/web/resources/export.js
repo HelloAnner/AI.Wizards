ac56c8a02813842c25d4c78ba2623c670ad445af
johntsai <johntsai@fanruan.com>
2023-07-29 13:59:22 +0800

    AI，设置内容

--- src/main/resources/com/fr/ai/wizards/web/resources/export.js
+++ src/main/resources/com/fr/ai/wizards/web/resources/export.js
@@ -5,6 +5,11 @@ BI.config("textarea.icon.click.handler", items => {
         if(task) {
             const templatePath = task.templatePath;
             console.log('定时调度选择的模板链接路径为:' + templatePath);
+
+            //todo 根据模板路径获取推荐的内容
+            const aiContent = 'AI推荐的内容测试XXXXXXX';
+            client.store.setContentText(aiContent);
+
         } else {
             console.warn('定时调度任务不存在');
         }
